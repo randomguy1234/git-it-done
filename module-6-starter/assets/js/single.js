bd6177c1bfe148b157693d52b3318cec64653644
Jimmy Rando <jsturat46925@gmail.com>
2022-01-06 01:53:23 -0500

    linked 2 html files together and prevented some errors

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,10 +1,32 @@
 var issueContainer= document.querySelector("#issues-container");
+var limitWarningEl= document.querySelector("#limit-warning");
+var repoNameEl= document.querySelector("#repo-name");
+
+var getRepoName= function()
+{
+    var queryString= document.location.search;
+    var repoName= queryString.split("=")[1];
+    console.log(repoName);
+    if (repoName)
+    {
+        // display repo name on the page
+        repoNameEl.textContent= repoName;
+        getRepoIssues(repoName);
+        
+    }
+    else
+    {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues= function(repo)
 {
     console.log(repo);
     var apiUrl= "https://api.github.com/repos/"+repo+"/issues?directions=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response){
         // request was successful
         if (response.ok)
@@ -13,11 +35,18 @@ var getRepoIssues= function(repo)
                 // pass response data to dom function
                 displayIssues(data);
                 //console.log(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link"))
+                {
+                    displayWarning(repo);
+                }
             });
         }
         else
         {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -65,4 +94,19 @@ var displayIssues= function(issues)
 
 };
 
-getRepoIssues("facebook/react");
+var displayWarning= function(repo)
+{
+    // add text to warning container
+    limitWarningEl.textContent= "To see more than 30 issues, visit ";
+
+    var linkEl= document.createElement("a");
+    linkEl.textContent= "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/"+repo+"/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+//getRepoIssues("facebook/react");
